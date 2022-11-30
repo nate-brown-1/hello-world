3b96f20b43bfc86eab440715f3f978ed78a8c8c5
Nate Brown <nathan.d.brown@gmail.com>
2022-11-30 12:25:39 -0800

    lab 8

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let userName;
 let speciesNumber;
 let userType;
+let pageScore;
 
 // declare function to get user name
 // validate for blank entry as "Anonymous", accept any other input
@@ -20,18 +21,35 @@ function getUserName() {
 
 // declare function to get species number
 // validate for speciesNumber >= 1, else call function again
+// function getSpeciesNumber() {
+//     speciesNumber = prompt("How many species are you shopping for?");
+//         if (speciesNumber == 1) {
+//             window.alert("No problem!");
+//         } else if (speciesNumber > 1) {
+//             window.alert("We have options for inhabitants of most systems!");
+//         } else {
+//             window.alert("That's not helpful!");
+//             getSpeciesNumber();
+//         }
+//         console.log(speciesNumber);
+//         return speciesNumber;
+// }
+
+// reimagine this function using a while loop instead of the recursive function from lab 7
+// it works but only says "that's not helpful" if input is number outside 1-100
+// accepts input of a string (not desired behavior)
+// the output of typeof() doesn't work as a condition for the while loop
 function getSpeciesNumber() {
-    speciesNumber = prompt("How many species are you shopping for?");
-        if (speciesNumber == 1) {
-            window.alert("No problem!");
-        } else if (speciesNumber > 1) {
-            window.alert("We have options for inhabitants of most systems!");
-        } else {
-            window.alert("That's not helpful!");
-            getSpeciesNumber();
-        }
-        console.log(speciesNumber);
-        return speciesNumber;
+     speciesNumber = prompt("How many species are you shopping for? (1-100)");
+         while (speciesNumber < 1 || speciesNumber > 100) {
+             window.alert("That's not helpful!");
+             console.log(speciesNumber);
+             speciesNumber = prompt("How many species are you shopping for?");
+         }
+         window.alert("Thank you for answering!");
+         console.log(speciesNumber);
+         console.log(typeof(speciesNumber));
+         return speciesNumber;
 }
 
 // declare function to get user type to show relevant specials
@@ -48,3 +66,13 @@ function getUserType() {
 }
 
 // all function calls are in index.html
+
+function rateThisPage() {
+    pageScore = prompt("Please rate this page (1-5 octopi)");
+    console.log(pageScore);
+
+    for (let i = 0; i < rating && i < 5; i++) {
+        console.log(i);
+        document.write('<img src="octopus.png" />');
+    }
+}
